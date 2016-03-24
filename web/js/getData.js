7603aac576ad685bf4849b9e6be546ecae2a5b90
Alberto <simioni.alberto@gmail.com>
2016-03-24 21:38:21 +0100

    Testo main page

--- web/js/getData.js
+++ web/js/getData.js
@@ -1,26 +1,21 @@
  var heatmapIntensity = 10;
 
  function getData(year1, year2, metric) {
-    console.log(metric)
     mapPos = {}
      testData = {
         max: heatmapIntensity,
         data: []
      }
-     console.log("getting data");
      var folder = null;
      if(metric === "avgTemp" || metric === "stddev") folder = "avgs"
      if(metric === "minTemp" || metric === "maxTemp") folder = "minmax"
-     loadJSON("results/"+folder+"/"+year2 + '.json',
+     loadJSON("results/"+folder+"/"+year2 + '.json', //second year data loading
          function (data) {
              fillData(data,mapPos,metric);
-             loadJSON("results/"+folder+"/"+year1 + ".json",
+             loadJSON("results/"+folder+"/"+year1 + ".json",  //first year data loading
                  function (data2) {
-                     calculateDifference(data2,mapPos,testData,metric);
-                     insertData(testData);
-
-                     console.log("finish");
-
+                     calculateDifference(data2,mapPos,testData,metric); //compute differences
+                     insertData(testData); //inserts the data in the heatmap
                  },
                  function (xhr) {
                      console.error(xhr);
@@ -36,27 +31,14 @@
 
  function fillData(data,mapPos,metric) {
      data.results.forEach(function (res) {
-
          mapPos[res.lat +"_" + res.long] = res[metric];
-         //testData.data.push({lat: res.latitude, lng:res.longitude, count: res.avgTemp})
      });
  }
 
  function calculateDifference(data2,mapPos,testData,metric) {
-     var min = 1000;
-     var max = -1000;
      data2.results.forEach(function (res) {
-
          if (mapPos[res.lat +"_" + res.long] !== undefined) {
-            //console.log(mapPos[res.lat +"_" + res.long])
              var diff = difference(mapPos[res.lat +"_" + res.long], res[metric]);
-             if (diff > max) {
-
-                 max = diff;
-             }
-             if (diff < min) {
-                 min = diff;
-             }
              //repeated for each region
              testData.data.push({
                  lat: res.lat,
@@ -65,9 +47,10 @@
              });
          }
      });
-
-     min = Math.abs(min);
-     console.log("min" + min + "  max:" + max);
+     //calculating the value to insert in the heatmap for each single data
+     //the values are between [0,10]
+     //if the difference is >= than 2.5 then it will have value 10
+     //The code is a bit weird in order to create the values for the gradient used in the heatmap
      testData.data.forEach(function (res) {
         var old = res.temp
         res.temp =  5 + res.temp*2
