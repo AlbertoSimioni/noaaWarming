f7652b705cfb0f54c1d7bd5acb74c51f25b087e2
Alberto <simioni.alberto@gmail.com>
2016-03-28 17:21:16 +0200

    Conclusions e mappa che mostra alzamento temperature

--- groupFiles.sh
+++ groupFiles.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-year="1949"
-for i in {0..0}
+
+#The script groupes the small files inside the dataset in the bigger files of the size of one HDFS block
+year="1901"
+for i in {0..115}
 do hadoop jar filecrush-2.2.2-SNAPSHOT.jar com.m6d.filecrush.crush.Crush -input-format text -output-format text -compress gzip -max-file-blocks 1 /user/lsde06/1949/ /user/lsde06/1949small/ 19491121121212 &
 done
